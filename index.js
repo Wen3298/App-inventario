f52c400b48b04d3d1c1bdd51ea93585a50855637
Wen3298 <wzelayavega@gmail.com>
2024-05-15 16:02:44 -0600

    LocalStorage

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const form =document.getElementById()
+const nombre = document.getElementById("name");
+const Cantidad = document.getElementById("eventcantidad");
+const Precio = document.getElementById("eventprecio");
+const formulario = document.getElementById("eventoformulario");
+const btnAgregar = document.getElementById("btnagregar");
+
+let datosLocalStorage = JSON.parse(localStorage.getItem("formulario"))|| []
+// Agregar un evento al formulario para guardar los datos en localStorage cuando se envíe//
+function guardarprodu() {
+
+    let datosproducto = {
+        nombre: nombre.value,
+        cantidad: Cantidad.value,
+        Precio: Precio.value
+    }
+    datosLocalStorage.push (datosproducto)
+
+    localStorage.setItem("formulario",JSON.stringify(datosLocalStorage))
+}
+   //aqui se guardan losdatos en localstorage//
+    localStorage.setItem("formulario", datosLocalStorage);
+
+// puse una alerta para ver si todo funciona y si se guardan los datos
+    alert("Producto Guadado");
+
+
+
+
